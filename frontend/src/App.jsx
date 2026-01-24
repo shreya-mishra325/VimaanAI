d2b8a408d5c187122c8e70e80558635326b761e8
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-01-24 12:27:15 +0530

    added cabin chime audio feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import Predict from "./pages/Predict.jsx";
@@ -9,6 +9,34 @@ import 'aos/dist/aos.css';
 
 
 function App() {
+  const audioRef = useRef(null);
+  const hasPlayedRef = useRef(false);
+
+  useEffect(() => {
+    audioRef.current = new Audio("/chime.mp3");
+    audioRef.current.volume = 0.9;
+  }, []);
+
+  useEffect(() => {
+    const playOnce = () => {
+      if (!hasPlayedRef.current && audioRef.current) {
+        audioRef.current.play().catch(() => {});
+        hasPlayedRef.current = true;
+
+        window.removeEventListener("click", playOnce);
+        window.removeEventListener("scroll", playOnce);
+      }
+    };
+
+    window.addEventListener("click", playOnce);
+    window.addEventListener("scroll", playOnce);
+
+    return () => {
+      window.removeEventListener("click", playOnce);
+      window.removeEventListener("scroll", playOnce);
+    };
+  }, []);
+
   useEffect(() => {
     AOS.init({
       duration:1000,
